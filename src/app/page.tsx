51b18a93b7d4eb826f0093c988bf6f64826628d0
Daytonbui <sutunhaymua@gmail.com>
2025-08-24 12:55:38 +0700

    Add error handling for score and time inputs in form submission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 interface FormState {
   score: number;
   time: number;
+  errors?: {
+    scrore?: string;
+    time?: string;
+  };
 }
 import { useActionState, useState } from "react";
 import Congratulation from "./components/Congratulation";
@@ -13,16 +17,31 @@ export default function Home() {
     { score: number; time: number }[]
   >([]);
   function handleSubmit(_previousState: FormState, formData: FormData) {
-    setScoreBoard((prev) => [
-      ...(prev || []),
-      {
-        score: formData.get("score") ? Number(formData.get("score")) : 0,
-        time: formData.get("time") ? Number(formData.get("time")) : 0,
-      },
-    ]);
+    const errors: { scrore?: string; time?: string } = {};
+    if (!formData.get("score")) {
+      errors.scrore = "Vui lòng nhập điểm";
+    }
+    if (!formData.get("time")) {
+      errors.time = "Vui lòng nhập thời gian";
+    }
+    if (!Object.keys(errors).length) {
+      setScoreBoard((prev) => [
+        ...(prev || []),
+        {
+          score: formData.get("score") ? Number(formData.get("score")) : 0,
+          time: formData.get("time") ? Number(formData.get("time")) : 0,
+        },
+      ]);
+      return {
+        score: 0,
+        time: 0,
+        errors: {},
+      };
+    }
     return {
       score: formData.get("score") ? Number(formData.get("score")) : 0,
       time: formData.get("time") ? Number(formData.get("time")) : 0,
+      errors,
     };
   }
   const [state, formAction, isPending] = useActionState<FormState, FormData>(
@@ -30,6 +49,7 @@ export default function Home() {
     {
       score: 0,
       time: 0,
+      errors: {},
     }
   );
   return (
@@ -38,12 +58,18 @@ export default function Home() {
         {/* input */}
         <aside className="flex flex-col gap-4 justify-center w-1/2 max-w-[500px] p-4">
           <div className="h-[100px]">
-            {!isPending && state.score > 0 && (
-              <>
-                <Congratulation score={state.score} />
-                <p>Điểm của bạn là: {state.score}</p>
-              </>
-            )}
+            {!isPending &&
+              scoreBoard.length &&
+              !Object.keys(state?.errors || {}).length && (
+                <>
+                  <Congratulation
+                    score={scoreBoard[scoreBoard.length - 1].score}
+                  />
+                  <p>
+                    Điểm của bạn là: {scoreBoard[scoreBoard.length - 1].score}
+                  </p>
+                </>
+              )}
           </div>
           <form action={formAction} className="flex flex-col gap-4">
             <label htmlFor="">
@@ -55,8 +81,12 @@ export default function Home() {
               min={1}
               max={100}
               step={1}
+              defaultValue={state.score || ""}
               className="border rounded px-3 py-2 text-center w-full"
             />
+            {state.errors?.scrore && (
+              <p className="text-red-500">{state.errors.scrore}</p>
+            )}
             <label htmlFor="">
               <span>Thời gian</span>
             </label>
@@ -65,8 +95,12 @@ export default function Home() {
               type="number"
               min={1}
               step={1}
+              defaultValue={state.time || ""}
               className="border rounded px-3 py-2 text-center w-full"
             />
+            {state.errors?.time && (
+              <p className="text-red-500">{state.errors.time}</p>
+            )}
             <button
               className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition-colors duration-200
               ${isPending ? "opacity-50 cursor-not-allowed" : ""}`}
